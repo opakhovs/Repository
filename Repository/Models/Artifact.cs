d38241b97d7107d381e8055d2d65c6f54daf441f
Vladyslav Boiko <vladboyko3112@gmail.com>
2019-04-13 20:41:41 +0300

    Added new models

---
+++ Repository/Models/Artifact.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Repository.Models
+{
+    public class Artifact
+    {
+        public int ArtifactId { get; set; }
+        public DateTime DateOfAdding { get; set; }
+        public string Version { get; set; }
+
+        public List<ArtifactProperty> Properties { get; set; }
+    }
+}
---
+++ Repository/Models/RepositoryContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Repository.Models
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext() : base("DbConnection") { }
+
+        public DbSet<Artifact> Artifacts { get; set; }
+        public DbSet<ArtType> ArtTypes { get; set; }
+        public DbSet<ProblemDomain> ProblemDomains { get; set; }
+        public DbSet<SubTask> SubTasks { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Rating> Ratings { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
