ffe800470b09fba2b6907e62db6bd89749e5093e
opakhovskiy <o.pahovskyi@polytech.software>
2020-03-27 01:06:40 +0200

    789987

---
+++ Repository/Controllers/NonFoundController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Repository.Controllers
+{
+    public class NonFoundController : Controller
+    {
+        // GET: NonFound
+        public ActionResult NotFound()
+        {
+            var result = new FilePathResult("~/Static/Pages/NotFound.html", "text/html");
+            return result;
+        }
+    }
+}
